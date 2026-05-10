e11e744480b8ec2212060e1b999ba1c29ad6e0fd
sleemanic <99089586+sleemanic@users.noreply.github.com>
2026-05-10 17:57:32 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
-const CACHE = 'shred-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+const CACHE = 'shred-v2';
+const ASSETS = [
+  '/shred-app/',
+  '/shred-app/index.html',
+  '/shred-app/manifest.json',
+  '/shred-app/icon.svg'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -15,6 +20,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/shred-app/index.html')))
   );
 });
